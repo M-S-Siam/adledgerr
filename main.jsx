b1b59f2ab47586e8aff12426177aba01275c3d5a
M-S-Siam <mdshahariar307@gmail.com>
2026-08-16 15:32:39 +0600

    Protect AdLytic with Supabase authentication

--- main.jsx
+++ main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AdLedgerApp from './App.jsx';
+import AuthGate from './src/AuthGate.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AdLedgerApp />
+    <AuthGate>
+      <AdLedgerApp />
+    </AuthGate>
   </React.StrictMode>
 );
