ae10522cb9b693553a9e942b71f0f470d4cda42f
M-S-Siam <mdshahariar307@gmail.com>
2026-08-27 22:00:03 +0600

    Load final auth placeholder fix after all auth styles

--- main.jsx
+++ main.jsx
@@ -4,6 +4,7 @@ import AdLedgerApp from './App.jsx';
 import AuthGate from './src/AuthGate.jsx';
 import './index.css';
 import './auth-fix.css';
+import './auth-placeholder-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
