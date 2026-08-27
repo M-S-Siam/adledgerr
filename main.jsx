bf3314bccfb91827c2913c51a83db92bb1be488c
M-S-Siam <mdshahariar307@gmail.com>
2026-08-27 22:34:58 +0600

    Apply professional Settings layout styles

--- main.jsx
+++ main.jsx
@@ -5,6 +5,7 @@ import AuthGate from './src/AuthGate.jsx';
 import './index.css';
 import './auth-fix.css';
 import './auth-placeholder-fix.css';
+import './settings-layout.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
