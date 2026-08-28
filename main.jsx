30f30f54de74ccabaff5fd9660dbfdd723e565ec
M-S-Siam <mdshahariar307@gmail.com>
2026-08-28 16:54:52 +0600

    Restore existing ledger data before rendering app

--- main.jsx
+++ main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AdLedgerApp from './App.jsx';
 import AuthGate from './src/AuthGate.jsx';
+import { recoverLedgerDataBeforeAppStarts } from './data-recovery-bootstrap.js';
 import './index.css';
 import './auth-fix.css';
 import './auth-placeholder-fix.css';
 import './settings-layout.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthGate>
-      <AdLedgerApp />
-    </AuthGate>
-  </React.StrictMode>
-);
+recoverLedgerDataBeforeAppStarts().finally(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <AuthGate>
+        <AdLedgerApp />
+      </AuthGate>
+    </React.StrictMode>
+  );
+});
